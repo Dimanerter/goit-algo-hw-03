a2a83f45ae64407081df3c3273bda2b1ac6dec1e
user name <user email>
2024-09-27 17:05:12 +0200

    final_1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_numbers_ticket(min, max, quantity):
     # Создание пустого списка для значений
     lottery_ticket = []
     # Проверка условий 
-    if min >= 1 and max <= 1000 and min < max:
+    if min >= 1 and max <= 1000 and min < max and quantity <= (max - min):
        # Создание цикла для заполнения списка всеми возможными числами
         while min < max:
             lottery_ticket.append(min)
@@ -50,6 +50,8 @@ def get_numbers_ticket(min, max, quantity):
 # Тестовые запуски
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
+# print(get_numbers_ticket(10, 15, 9))
+# print(get_numbers_ticket(10, 4, 2))
 # print(get_numbers_ticket(10, 4, 2))
 # print(get_numbers_ticket(-1, 10000, 6))
 # print(get_numbers_ticket(90, 100, 6))
